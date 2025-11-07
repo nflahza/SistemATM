0d35fcbabaf70a7a1c06b533d8c7ea6dbfe0b374
NaFl <72364452+nflahza@users.noreply.github.com>
2025-11-07 19:35:28 +0700

    initial commit

---
+++ SistemATM.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class SistemATM {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sistem ATM");
+    }
+}
